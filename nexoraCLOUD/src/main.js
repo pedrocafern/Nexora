e73152fed064f243b400fbf50b91d2f5d1b6dfa6
Desenvolvimento <dsadm@educ501.local>
2026-03-03 19:21:39 -0300

    Atualização (Adicionando Vuetify)

--- nexoraCLOUD/src/main.js
+++ nexoraCLOUD/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import 'vuetify/styles'
+import './styles.css' 
+import { createVuetify } from 'vuetify'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify()
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
