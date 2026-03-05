1d159999c1614bea39a16123634b0b62298b5b17
pedrocafern <pedrocafern@gmail.com>
2026-03-05 20:57:36 -0300

    Sim

--- nexoraCLOUD/src/App.vue
+++ nexoraCLOUD/src/App.vue
@@ -5,12 +5,17 @@ import { RouterLink,  RouterView} from 'vue-router'
 <template>
   <v-app>
     <v-app-bar>
+
       <v-toolbar-title>NexoraCLOUD</v-toolbar-title>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
+    
     </v-app-bar>
+
+    <v-main>
+      <v-container>
+         <RouterView />
+      </v-container>
+    </v-main>
   </v-app>
-  <main>
-    <RouterView />
-  </main>
 </template>
--- nexoraCLOUD/src/main.js
+++ nexoraCLOUD/src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
 import router from './router'
 
-import { vuetify } from 'vite-plugin-vuetify'
-
 const app = createApp(App)
+
 app.use(vuetify)
 app.use(router)
 app.mount('#app')
