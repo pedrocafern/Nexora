cf105d6b8e8aea32babad28ba64843b0a2c9fbfa
pedrocafern <pedrocafern@gmail.com>
2026-03-15 14:58:35 -0300

    Correções.

--- nexoraCLOUD/src/components/Navbar.vue
+++ nexoraCLOUD/src/components/Navbar.vue
@@ -20,3 +20,52 @@ import logo from '../assets/Logo.svg'
         </nav>
     </header>
 </template>
+
+<style scoped>
+
+.logo {
+    width: 180px;
+}
+
+.navBar{
+  display:flex;
+  align-items:center;
+  justify-content:space-between;
+
+  margin:auto;
+
+  padding:1rem 40px;
+
+  background: rgba(20,20,20,0.45);
+  backdrop-filter: blur(10px);
+}
+
+.navLinks {
+  /* Links */
+  display: flex;
+  align-items: center;
+  gap: 1.5rem;
+}
+
+.navButtonLink{
+  padding: 8px 18px;
+  border-radius: 6px;
+  background: linear-gradient(135deg, var(--color-orange), var(--color-yellow));
+  color: black;
+  font-weight: 600;
+  transition: 0.25s ease;
+}
+
+.navButtonLink:hover {
+  transform: translateY(-1px);
+  box-shadow: 0 4px 12px rgba(255,140,26,0.35);
+}
+
+a {
+  /* Tirar o traço embaixo do link */
+  text-decoration: none;
+  /* Cor dos links */
+  color: var(--text-color);
+}
+
+</style>
--- nexoraCLOUD/src/views/Home.vue
+++ nexoraCLOUD/src/views/Home.vue
@@ -19,3 +19,99 @@
 
   </section>
 </template>
+
+<style scoped>
+
+.hero{
+height: calc(100vh - 70px);
+
+display:flex;
+align-items:center;
+justify-content:center;
+}
+
+.heroContent{
+padding: 75px;
+border-radius: 50px;
+max-width: 600px;
+text-align: left;
+background: rgba(20,20,20,0.45);
+backdrop-filter: blur(10px);
+}
+
+.heroContent h1{
+font-size:3rem;
+margin-bottom:20px;
+}
+
+.heroContent p{
+font-size:1.2rem;
+opacity:0.8;
+margin-bottom:30px;
+}
+
+.heroButtons{
+display:flex;
+gap:15px;
+}
+/* Primeiro botão */
+.primary{
+padding:12px 28px;
+border-radius:8px;
+
+/* Cria um efeito de gradiente no botão */
+background: linear-gradient(
+135deg,
+var(--color-orange),
+var(--color-yellow)
+);
+
+border:none;
+color:black;
+font-weight:600;
+
+/* Define o que vai acontecer no Hover. */
+cursor:pointer;
+transition: all 0.25s ease;
+}
+/* Efeitos (Animação) do primeiro botão */
+.primary:hover{
+transform: translateY(-2px);
+
+box-shadow:
+0 8px 25px rgba(255,140,26,0.35);
+}
+
+.secondary{
+/* Espaço entre os botões de forma geral */
+padding:12px 26px;
+
+border-radius:8px;
+
+background: rgba(20,20,20,0.35);
+
+/*  Cria uma borda em torno do botão */
+border:1px solid rgba(255,255,255,0.15);
+
+color:white;
+font-weight:500;
+
+backdrop-filter: blur(8px);
+
+cursor:pointer;
+
+transition: all 0.25s ease;
+}
+/* Efeitos (Animação) do segundo botão */
+.secondary:hover{   /* Hover, aplica uma animação após o mouse parar sobre o botão */
+background: rgba(20,20,20,0.55);
+
+border:1px solid rgba(255,255,255,0.25);
+
+transform: translateY(-2px);
+
+box-shadow:
+0 6px 20px rgba(0,0,0,0.35);
+}
+
+</style>
